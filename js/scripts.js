88560d2c60844e0c946b0fd3574b11d5fdedf4e7
Julien L <julienlenaz96@gmail.com>
2023-07-11 00:16:57 -0400

    Added pig dice rules and holding functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,60 +3,63 @@
 function PlayerScores() {
   this.playerRolls = [];
   this.playerScore = 0;
-  this.currentId = 0;
 }
 
-PlayerScores.prototype.addScore = function(roll) {
-  roll.id = this.assignId();
-  this.playerRolls.push(roll);
-};
-
-PlayerScores.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-};
-
-PlayerScores.prototype.diceRoll = function() {
+PlayerScores.prototype.rollDice = function() {
   var diceRoll = Math.floor(Math.random() * 6) + 1;
-  if (diceRoll > 1) {
-    this.playerScore += diceRoll;
-    if (this.playerScore > 100) {
-      return "You win!";
-    } else {
-      return diceRoll;
-    }
+  if (diceRoll === 1) {
+    this.playerScore = 0;
+    return "Oops! You rolled a 1.";
   } else {
-    return "End of Turn";
+    this.playerScore += diceRoll;
+    return diceRoll;
   }
+};
+
+PlayerScores.prototype.holdScore = function() {
+  // Add the current player's score to their total score
+  var totalScore = this.playerScore;
+  this.playerScore = 0;
+  return totalScore;
 };
 
 
 // UI LOGIC //
 
-
 let playerOneScores = new PlayerScores();
 let playerTwoScores = new PlayerScores();
+let currentPlayerScores = playerOneScores;
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const currentPlayer = getCurrentPlayer();
-  const roll = currentPlayer.diceRoll();
-  currentPlayer.addScore(roll);
+  const rollResult = currentPlayerScores.rollDice();
+  currentPlayerScores.playerRolls.push(rollResult);
+  
+  if (rollResult === "Oops! You rolled a 1.") {
+    currentPlayerScores = toggleCurrentPlayer();
+  }
+
   updatePlayerScoresUI();
 }
 
-function getCurrentPlayer() {
-  const totalRolls = playerOneScores.playerRolls.length + playerTwoScores.playerRolls.length;
-  if (totalRolls % 2 === 0) {
-    return playerOneScores;
+function toggleCurrentPlayer() {
+  if (currentPlayerScores === playerOneScores) {
+    currentPlayerScores = playerTwoScores;
   } else {
-    return playerTwoScores;
+    currentPlayerScores = playerOneScores;
   }
+  return currentPlayerScores;
 }
 
 function updatePlayerScoresUI() {
   listPlayerScores(playerOneScores, "playerOneScores", "Player One Rolls");
   listPlayerScores(playerTwoScores, "playerTwoScores", "Player Two Rolls");
+  const holdButton = document.getElementById('holdButton'); 
+  const holdButtonContainer = holdButton.parentNode;
+  holdButtonContainer.classList.remove('current-player');
+  if (currentPlayer === playerOneScores) {
+  holdButtonContainer.classList.add('current-player');
+}
 }
 
 function listPlayerScores(playerScores, playerScoresDivId, headingText) {
@@ -71,20 +74,16 @@ function listPlayerScores(playerScores, playerScoresDivId, headingText) {
 
   const h2 = document.createElement("h2");
   h2.textContent = headingText;
-  if (isCurrentPlayer(playerScores)) {
+  if (playerScores === currentPlayerScores) {
     h2.classList.add("current-player");
   }
   column.appendChild(h2);
 
   const h3 = document.createElement("h3");
-  if (playerScores.playerScore >= 100) {
-    h3.innerText = "You win!";
-  } else {
-    h3.innerText = "Current Score: " + playerScores.playerScore;
-  }
+  h3.innerText = "Current Score: " + playerScores.playerScore;
   column.appendChild(h3);
 
-  playerScores.playerRolls.forEach(function(roll) {
+  playerScores.playerRolls.forEach(function (roll) {
     const div = document.createElement("div");
     div.classList.add("roll-box");
 
@@ -95,11 +94,16 @@ function listPlayerScores(playerScores, playerScoresDivId, headingText) {
     column.appendChild(div);
   });
 
+  const holdButton = document.createElement("button");
+  holdButton.innerText = "Hold";
+  holdButton.addEventListener("click", function() {
+    const totalScore = playerScores.holdScore();
+    alert("Score held! Total score: " + totalScore);
+    currentPlayerScores = toggleCurrentPlayer();
+    updatePlayerScoresUI();
+  });
+
+  column.appendChild(holdButton);
   columnContainer.appendChild(column);
   playerScoresDiv.appendChild(columnContainer);
-}
-
-function isCurrentPlayer(playerScores) {
-  const currentPlayer = getCurrentPlayer();
-  return playerScores === currentPlayer;
 }
