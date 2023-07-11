7fc769ab95cbbe1e1ac9eebb1dc90ffc540aad4a
Julien L <julienlenaz96@gmail.com>
2023-07-10 20:06:34 -0400

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+// BUSINESS LOGIC FOR PLAYER SCORES //
+
+function PlayerScores() {
+this.playerRolls = [];
+this.playerScore = 0;
+this.currentId = 0;
+}
+
+PlayerScores.prototype.addScore = function(roll) {
+roll.id = this.assignId();
+this.playerRolls.push(roll);
+};
+
+PlayerScores.prototype.assignId = function() {
+this.currentId += 1;
+return this.currentId;
+};
+
+PlayerScores.prototype.diceRoll = function() {
+var diceRoll = Math.floor(Math.random() * 6) + 1;
+if (diceRoll > 1) {
+this.playerScore += diceRoll;
+if (this.playerScore > 100) {
+return "You win!";
+} else {
+return this.playerScore;
+}
+} else {
+return "End of Turn";
+}
+};
+
+
+
+// UI LOGIC //
+
+let playerOneScores = new PlayerScores();
+let playerTwoScores = new PlayerScores();
+
+function handleFormSubmission(event) {
+event.preventDefault();
+const diceRoll = document.getElementById("diceRoll").value;
+function getCurrentPlayer() {
+  const totalRolls = playerOneScores.playerRolls.length + playerTwoScores.playerRolls.length;
+  
+  if (totalRolls % 2 === 0) {
+    return playerOneScores;
+  } else {
+    return playerTwoScores;
+  }
+}
+const currentPlayer = getCurrentPlayer();
+currentPlayer.addScore(diceRoll);
+updatePlayerScoresUI();
+}
+
+function updatePlayerScoresUI() {
+listPlayerScores(playerOneScores, "playerOneScores");
+listPlayerScores(playerTwoScores, "playerTwoScores");
+}
+
+function listPlayerScores(playerScores, playerScoresDivId) {
+let playerScoresDiv = document.querySelector("div#" + playerScoresDivId);
+playerScoresDiv.innerHTML = "";
+const ul = document.createElement("ul");
+
+playerScores.playerRolls.forEach(function(roll) {
+const li = document.createElement("li");
+li.innerText = roll;
+ul.append(li);
+});
+
+playerScoresDiv.append(ul);
+}
